1eaa9f34ca24963ebdac57fd27d2c7e30e381218
Alex Coronell <alexcoronell@gmail.com>
2019-10-29 10:11:13 -0500

    Terminado ajuste de menu desktop por scroll

--- js/main.js
+++ js/main.js
@@ -1,46 +1,56 @@
-//************************ SLIDE / INFOPROMOCIONES *************/
-var slideIndex = 0;
-showSlides();
+/************************* Menu Scroll ****************/
+window.onscroll = function() {
+    // Obtenemos la posicion del scroll en pantalla
+    var scroll = document.documentElement.scrollTop || document.body.scrollTop;
+    var menuSombra = document.getElementById('menu-desktop-sombra');
+    var barraMenu = document.getElementById('menu-desktop');
+    var buttonMenu = document.getElementById('boton-menu-desktop');
+    var menuEspecial = document.getElementById('menu-desktop-special');
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    // Realizamos alguna accion cuando el scroll este entre la posicion 300 y 400
+    if (scroll >= 100) {
+        menuSombra.classList.add('ocultarsombra');
+        barraMenu.classList.add('menudesktop-scroll');
+        buttonMenu.classList.add('menu-scroll-boton');
+        menuEspecial.classList.add('reubicarmenu');
+    } else {
+        menuSombra.classList.remove('ocultarsombra');
+        barraMenu.classList.remove('menudesktop-scroll');
+        buttonMenu.classList.remove('menu-scroll-boton');
+        menuEspecial.classList.remove('reubicarmenu');
     }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
 
+//************************ SLIDE / INFOPROMOCIONES *************/
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) { slideIndex = 1 }
+//     slides[slideIndex - 1].style.display = "block";
+//     setTimeout(showSlides, 4000); // Change image every 4 seconds
+// }
+
 /************************* Iniciamos smoothScroll (Scroll Suave) ****************/
-smoothScroll.init({
-    speed: 1000, // Integer. How fast to complete the scroll in milliseconds
-    offset: 100, // Integer. How far to offset the scrolling anchor location in pixels
+// $(document).ready() {
+//     smoothScroll.init({
+//         speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+//         offset: 100, // Integer. How far to offset the scrolling anchor location in pixels
 
-});
+//     });
+// }
 
-/************************* Ocultar sombra de menu ****************/
-$(window).scroll(function() {
 
-    var div = $('#menu-desktop-sombra');
-    var barraMenu = $('#menu-desktop');
-    var buttonMenu = $('#boton-menu-desktop');
-    var menuEspecial = $('#menu-desktop-special');
-    var scroll = $(window).scrollTop();
 
-    if (scroll >= 100) {
-        div.addClass("ocultarsombra");
-        barraMenu.addClass("menudesktop-scroll");
-        buttonMenu.addClass("menu-scroll-boton");
-        menuEspecial.addClass("reubicarmenu");
-    } else {
-        div.removeClass("ocultarsombra");
-        barraMenu.removeClass("menudesktop-scroll");
-        buttonMenu.removeClass("menu-scroll-boton");
-        menuEspecial.removeClass("reubicarmenu");
-    }
-});
 
-/************************* Menu Scroll ****************/
+/*************************  ****************/
+
+// function desplazaTop() {
+//     window.scrollTo(0, 0);
+// }
